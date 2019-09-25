f0569813b79974a6e1ee9646fa884f65ed546fce
wasit shafi <wasitshafi700@gmail.com>
2019-09-26 02:23:12 +0530

    added files

---
+++ c++/printAllPossibleSubStrings.cpp
@@ -0,0 +1,25 @@
+//
+//printAllPossibleSubStrings.cpp
+//
+//program to print all possible sub-strings of a given string.
+//
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout << endl << "Enter string : ";
+    getline(cin, str);
+
+    cout << "All Possible Sub-Strings are as : " << endl << endl;
+    for(int i = 0 ; i < str.size() ; i++)
+    {
+        for( int len = 1 ; len <= str.size() - i; len++)
+            cout << "\'" << str.substr(i, len) << "\'  ";
+        cout << endl ;
+    }
+    
+    return 0;
+}
